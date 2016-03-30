caf2b2d64c812ccf68f333aa4f64d26e2349d1c2
Matthew Rumery <matt.superman@gmail.com>
2016-03-30 10:39:06 -0600

    add update blank course sis ids

---
+++ api/update_blank_course_sis_ids/README.md
@@ -0,0 +1,9 @@
+# Update Blank Course SIS ID Values
+
+The first column needs to be named `canvas_course_id`, this will be the Canvas ID that is displayed in the URL of the course.
+
+The second column need to be named `new_course_id`, this will be the new SIS ID for the course.
+
+Please see the `example.csv` as a template to structure your CSV file.
+
+*Note:* Both column ID values must be the SIS ID of the course.
---
+++ api/update_blank_course_sis_ids/update_blank_course_sis_ids.rb
@@ -0,0 +1,57 @@
+#Working as of 03/30/2016
+require 'typhoeus'
+require 'csv'
+require 'json'
+
+################### Change these values only ##################
+access_token = ''				#Your access token for Canvas
+domain = ''							#The should be the first part of the url
+env = nil								#Either blank for prod, or type test or beta
+csv_file = ''						#The full path to the location of the mapping file /full/path/to/the/file.csv
+################### Do not change these #######################
+
+default_headers = { 'Authorization' => "Bearer #{access_token}"}
+
+hydra = Typhoeus::Hydra.new(max_concurrency: 40)
+
+env ? env << '.' : env
+base_url = "https://#{domain}.#{env}instructure.com/"
+
+CSV.foreach(csv_file, {headers: true}) do |row|
+	if row.headers[0] != 'canvas_course_id' || row.headers[1] != 'new_course_id'
+		puts "First column needs to be 'canvas_course_id', and second row needs to be 'new_course_id'."
+	else
+	get_course = Typhoeus::Request.new(base_url + "api/v1/accounts/self/courses/#{row['canvas_course_id']}",
+									  method: :get,
+									  headers: default_headers)
+	get_course.on_complete do |response|
+		if response.success?
+			update_course = Typhoeus::Request.new(base_url + "api/v1/courses/#{row['canvas_course_id']}",
+												  method: :put,
+												  headers: default_headers,
+												  params: { 'course[sis_course_id]' => row['new_course_id']})
+				update_course.on_complete do |response|
+					if response.success?
+						puts "Updated old course number of #{row['canvas_course_id']} with SIS ID of #{row['new_course_id']}"
+					elsif response.timed_out?
+				        puts "Unable to find the course #{row['canvas_course_id']}, response timed out"
+					elsif response.code == 0
+				        puts response.return_message
+					else
+				    	puts "HTTP request failed: " + response.code.to_s
+					end
+				end
+				hydra.queue(update_course)
+		elsif response.timed_out?
+			puts "Unable to find the course #{row['canvas_course_id']}, response timed out"
+		elsif response.code == 0
+			puts response.return_message
+		else
+			puts "HTTP request failed: " + response.code.to_s
+		end
+	end
+	hydra.queue(get_course)
+end
+	hydra.run
+end
+	puts 'Completed blank course SIS updates'
