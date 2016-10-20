b2b8dce35a45866f3b478f712ee7a4f7db7e7f8b
Matthew Rumery <mrumery@instructure.com>
2016-10-20 09:40:51 -0600

    Change bulk export to use Typhoeus, resolve nil error

--- api/bulk_course_export/ruby/bulk_course_export.rb
+++ api/bulk_course_export/ruby/bulk_course_export.rb
@@ -4,12 +4,13 @@ access_token = nil          #your token acquired in your canvas user profile
 domain = nil                #the domain of your URL, such as school in school.instructure.com
 env = nil                   #leave nil for prod, or enter test & beta
 input_file = nil            #input file for courses to export
-output_file = nil           #output file for courses that have been exported
+output_file = nil            #output file for courses that have been exported
 #============
 # Don't edit from here down unless you know what you're doing.
 
-require 'unirest'
+require 'typhoeus'
 require 'csv'
+require 'json'
 
 unless access_token
   puts "what is your access token?"
@@ -41,42 +42,37 @@ unless File.exists?(output_file)
   end
 end
 
-env ? env << "." : env
+env ? env << '.' : env
 base_url = "https://#{domain}.#{env}instructure.com/api/v1"
-test_url = "#{base_url}/accounts/self"
+default_header = { Authorization: "Bearer #{access_token}" }
 
-Unirest.default_header("Authorization", "Bearer #{access_token}")
+CSV.foreach(input_file, { headers: true }) do |row|
+  url = "#{base_url}/courses/#{row['course_id']}/content_exports"
+  export_course = Typhoeus::Request.new(url, headers: default_header, method: :post,
+                                        params: { export_type: 'common_cartridge', skip_notifications: true })
 
-# Make generic API call to test token, domain, and env.
-test = Unirest.get(test_url)
+  export_course.on_complete do |response|
+    response_body = JSON.parse(response.body)
+    while (response_body['workflow_state'] == 'created' || response_body['workflow_state'] == 'exporting' ) && (response_body['progress'] != 100)
+      puts 'exporting'
+      sleep(5)
+      export_status = Typhoeus.get("#{url}/#{response_body['id']}", headers: default_header)
+      response_body = JSON.parse(export_status.body)
+    end
 
-unless test.code == 200
-  raise "Error: The token, domain, or env variables are not set correctly"
+    if response_body['processing_errors']
+      puts "An error occurred exporting this job. -- #{response_body}"
+    end
+
+    if response_body['processing_warnings']
+      puts "Processing Warning: #{response_body['processing_errors']}"
+    end
+
+    CSV.open(output_file, 'ab') do |csv|
+      csv << ["#{response_body['course_id']}", "#{response_body['attachment']['url']}"]
+    end
+  end
+  export_course.run
 end
 
-CSV.foreach(input_file, {:headers => true}) do |cid|
-  url ="#{base_url}/courses/#{cid['course_id']}/content_exports"
-  export_course = Unirest.post(url, parameters: { "export_type" => "common_cartridge", "skip_notifications" => 1 })
-  job = export_course.body
-
-  while job["workflow_state"] == "created" || job["work_flow_state"] == "importing"
-    puts "exporting"
-    sleep(3)
-    export_status = Unirest.get("#{url}/#{job['id']}")
-    job = export_status.body
-  end
-
-  if job["processing_errors"]
-    puts "An error occurred exporting this job. \n #{job}"
-  end
-
-  if job["processing_warnings"]
-    puts "Processing Warning: #{job["processing_errors"]}"
-  end
-
-  CSV.open(output_file, 'ab') do |csv|
-    csv << ["#{job['course_id']}", "#{job['attachment']['url']}"]
-  end
-end
-
-puts "Successfully exported files."
+puts "Exported Files."
