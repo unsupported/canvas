9450ec7591a6c9dc59ebeb6a899a9d7cde4672b0
Brock Halladay <bhalladay@instructure.com>
2017-04-04 15:15:58 -0600

    updated print button script

--- branding/add_print_button_syllabus/add_print_button.js
+++ branding/add_print_button_syllabus/add_print_button.js
@@ -3,7 +3,8 @@
 
 $(document).ready(function() {
     if (window.location.href.indexOf("syllabus") > -1) {
-        var new_link = $('<div class="grid-row middle-xs between-xs" style="margin-bottom: 10px"><div class="col-xs-6"></div><div class="col-md-6 col-lg-3"><a class="btn print-grades icon-printer" href="javascript:window.print()">Print Syllabus</a></div></div>');
+        var new_link = $('<div id="printButton" class="grid-row middle-xs between-xs" style="margin-bottom: 10px"><div class="col-xs-6"></div><div class="col-md-6 col-lg-3"><a class="btn print-grades icon-printer" href="javascript:window.print()">Print Syllabus</a></div></div>');
         $('#content').prepend(new_link);
+        $('head').append('<style>@media print {#printButton {display:none !important;}}</style>');
     }
 });
