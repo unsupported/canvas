bbbf718e29f07dc9f67301e799f88b5eca01e5d3
Brock Halladay <bhalladay@instructure.com>
2017-07-26 13:32:22 -0600

    Added a script that will show the activity time if the user is a student

---
+++ branding/total_activity_student_view/student_activity_time.js
@@ -0,0 +1,37 @@
+/*working as of July 26th, 2017*/
+//Script to add the total activity time for the course a student is on
+
+$(document).ready(function() {
+  function convertSeconds(totalSeconds) {
+    totalSeconds = Number(totalSeconds);
+
+    let d = Math.floor(totalSeconds / 86400);
+    let h = Math.floor(totalSeconds % 86400 / 3600);
+    let m = Math.floor(totalSeconds % 86400 % 3600 / 60);
+    let s = Math.floor(totalSeconds % 86400 % 3600 % 60);
+
+    return (' ' + d).slice(d.length) + ' day(s) ' + ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+  }
+
+  if (window.location.pathname.indexOf('courses') > -1 && $.inArray('student', ENV.current_user_roles) > -1) {
+    let userId = ENV.current_user_id;
+    let courseId = ENV.COURSE_ID;
+    let jsonUrl = `https://${window.location.hostname}/api/v1/users/${userId}/enrollments`;
+    let time = "";
+
+    $.getJSON(jsonUrl, (data) => {
+      let currentCourse = data.filter((course) => {
+        return course.course_id == courseId
+      });
+      // console.log(currentCourse);
+      time = currentCourse[0].total_activity_time;
+      // console.log(time);
+      time = convertSeconds(time);
+      // console.log(time);
+      $('#right-side').prepend("<div class='events_list time_active'><h2>Time Activity</h2></div>");
+      $(".events_list.time_active").append(time + " (hrs/mins/secs)");
+    });
+
+  }
+
+});
