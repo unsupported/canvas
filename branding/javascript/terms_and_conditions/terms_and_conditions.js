82f64c5b4707929eeb10f95a57c33016006bc759
Brock Halladay <bhalladay@instructure.com>
2017-08-04 12:48:21 -0600

    minor fixes to long line in terms_and_conditions.js file

--- branding/javascript/terms_and_conditions/terms_and_conditions.js
+++ branding/javascript/terms_and_conditions/terms_and_conditions.js
@@ -10,7 +10,12 @@ let privacy = "https://example.org"; //insert URL to privacy policy
 //Don't edit past here unless you know JS and are comfortable
 //making changes on your own.
 $(document).ready(() => {
-  $('label.checkbox input[name="user[terms_of_use]"]').parent().parent().append(`<p><label class="checkbox"><input type="checkbox" name="second[terms_of_use]">I agree to the <a target="_blank" href=${terms} class="external" rel="noreferrer"><span>client's terms of use</span><span class="ui-icon ui-icon-extlink ui-icon-inline" title="Links to an external site."><span class="screenreader-only">Links to an external site.</span></span></a> and <a target="_blank" href=${privacy} class="external" rel="noreferrer"><span>privacy policy</span><span class="ui-icon ui-icon-extlink ui-icon-inline" title="Links to an external site."><span class="screenreader-only">Links to an external site.</span></span></a>.</label></p>`);
+  $('label.checkbox input[name="user[terms_of_use]"]').parent().parent().append(`<p><label class="checkbox">\
+  <input type="checkbox" name="second[terms_of_use]">I agree to the <a target="_blank" href=${terms} class="external" rel="noreferrer">\
+  <span>client's terms of use</span><span class="ui-icon ui-icon-extlink ui-icon-inline" title="Links to an external site.">\
+  <span class="screenreader-only">Links to an external site.</span></span></a> and <a target="_blank" href=${privacy} class="external" rel="noreferrer">\
+  <span>privacy policy</span><span class="ui-icon ui-icon-extlink ui-icon-inline" title="Links to an external site."><span class="screenreader-only">\
+  Links to an external site.</span></span></a>.</label></p>`);
   $('.button_box.ic-Login-confirmation__actions button[type="submit"]').on('click', (e) => {
     if ($('[name="user[terms_of_use]"]').prop('checked') && $('[name="second[terms_of_use]"]').prop('checked')) {
       console.log("both terms accepted");
