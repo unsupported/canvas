76e28fd658e5ebda9e5ee3006c429e29845b74e1
Colin Cromar <ccromar@instructure.com>
2017-09-30 10:39:14 -0600

    organizes branding folder and polishes outcomes_exporter

--- api/outcomes_exporter/outcome_exporter.rb
+++ api/outcomes_exporter/outcome_exporter.rb
@@ -1,4 +1,4 @@
-# LAST WORKING AS OF 08/02/2017
+# LAST WORKING AS OF 09/30/2017
 
 require 'csv'
 require 'json'
@@ -20,7 +20,7 @@ require 'ruby-progressbar'
 
 def fetch_outcomes
   outcomes = []
-  ids = [*1..1000]
+  ids = [*1..2500]
 
   puts 'Fetching outcomes...'
 
@@ -28,20 +28,21 @@ def fetch_outcomes
 
   ids.each do |id|
     outcome = {}
-    resp = Typhoeus.get(@base_url + "/outcomes/" + id.to_s,
-            headers: { authorization: "Bearer #{@token}" })
+    resp = Typhoeus.get(@base_url + '/outcomes/' + id.to_s,
+                        headers: { authorization: "Bearer #{@token}" })
     next unless resp.code == 200
     data = JSON.parse(resp.body)
 
-    outcome[:vendor_guid] = data['vendor_guid']
     outcome[:outcome_group_vendor_guid] = ''
     outcome[:parent_outcome_group_vendor_guid] = ''
-    outcome[:title] = data['title']
-    outcome[:ratings] = data['ratings']
-    outcome[:description] = data['description']
+
+    outcome[:vendor_guid]        = data['vendor_guid']
+    outcome[:title]              = data['title']
+    outcome[:ratings]            = data['ratings']
+    outcome[:description]        = data['description']
     outcome[:calculation_method] = data['calculation_method']
-    outcome[:calculation_int] = data['calculation_int']
-    outcome[:mastery_points] = data['mastery_points']
+    outcome[:calculation_int]    = data['calculation_int']
+    outcome[:mastery_points]     = data['mastery_points']
     outcomes << outcome
 
     progressbar.increment
@@ -51,29 +52,31 @@ def fetch_outcomes
 end
 
 def create_csv
-  headers = "vendor_guid,outcome_group_vendor_guid,parent_outcome_group_vendor_guid," \
+  headers = 'vendor_guid,outcome_group_vendor_guid,parent_outcome_group_vendor_guid,' \
   "title,description,calculation_method,calculation_int,mastery_points,0,1,2,3,4,5,6,7,8,9,10\n"
 
   File.open("./#{@domain}_outcomes_export_log.csv", 'w'){ |x| x.write(headers) }
 end
 
 def print_to_csv(outcomes)
-  headers = %w[vendor_guid outcome_group_vendor_guid parent_outcome_group_vendor_guid title description calculation_method calculation_int mastery_points 0 1 2 3 4 5 6 7 8 9 10]
+  headers = %w[vendor_guid outcome_group_vendor_guid parent_outcome_group_vendor_guid
+               title description calculation_method calculation_int mastery_points
+               0 1 2 3 4 5 6 7 8 9 10]
   outcomes.each do |oc|
-    CSV.open("./#{@domain}_outcomes_export_log.csv","a") do |csv|
+    CSV.open("./#{@domain}_outcomes_export_log.csv",'a') do |csv|
       row = CSV::Row.new(headers, [])
-      row['vendor_guid'] = oc[:vendor_guid]
-      row['outcome_group_vendor_guid'] = oc[:outcome_group_vendor_guid]
+      row['vendor_guid']                      = oc[:vendor_guid]
+      row['outcome_group_vendor_guid']        = oc[:outcome_group_vendor_guid]
       row['parent_outcome_group_vendor_guid'] = oc[:parent_outcome_group_vendor_guid]
-      row['title'] = oc[:title]
-      row['description'] = oc[:description]
-      row['calculation_method'] = oc[:calculation_method]
-      row['calculation_int'] = oc[:calculation_int]
-      row['mastery_points'] = oc[:mastery_points]
+      row['title']                            = oc[:title]
+      row['description']                      = oc[:description]
+      row['calculation_method']               = oc[:calculation_method]
+      row['calculation_int']                  = oc[:calculation_int]
+      row['mastery_points']                   = oc[:mastery_points]
       oc[:ratings].each do |rating|
-        description = rating['description']
-        points_as_int = rating['points'].to_i
-        row["#{points_as_int}"] = description
+        description             = rating['description']
+        points_as_int           = rating['points'].to_i
+        row[points_as_int.to_s] = description
       end
       csv << row
     end
