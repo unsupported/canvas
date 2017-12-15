e45db7064c3f3f4c9ff35b5cb50f39de52a7fbbc
Colin Cromar <colincromar@gmail.com>
2017-12-15 10:15:43 -0700

    updates documentation for merge users script (#86)

--- api/merge_multiple_users/ruby/merge_multiple_users.rb
+++ api/merge_multiple_users/ruby/merge_multiple_users.rb
@@ -1,39 +1,33 @@
-# Working as of 03/24/2016
+# Working as of 12/15/2017
 
 require 'csv'
 require 'typhoeus'
 require 'json'
 #------------------Replace these values-----------------------------#
 
-access_token = ''
-url = 'canvas.instructure.com'  			                          #Enter the full URL to the domain you want to merge files.
-csv_file = 'full/path/to/the/file.csv' 													#Enter the full path to the file. /Users/XXXXXX/Path/To/File.csv
+access_token = '' # API token
+url = 'https://canvas.instructure.com' # Enter the full URL to the domain you want to merge files. Must begin with https://
+csv_file = 'full/path/to/the/file.csv' # Enter the full path to the file. /Users/XXXXXX/Path/To/File.csv
 
 #-------------------Do not edit below this line---------------------#
 # First column is user account that will be merged into second column. #
-unless Typhoeus.get(url).code == 200 || 302
-	raise 'Unable to run script, please check token, and/or URL.'
-end
+raise 'Unable to run script, please check token, and/or URL.' unless Typhoeus.get(url).code == 200 || 302
 
-unless File.exists?(csv_file)
-	raise "Can't locate the CSV file."
-end
+raise "Can't locate the CSV file." unless File.exist?(csv_file)
 
 hydra = Typhoeus::Hydra.new(max_concurrency: 10)
 
 CSV.foreach(csv_file, { headers: true }) do |row|
 
-	api_call = "#{url}/api/v1/users/sis_user_id:#{row['to_be_merged']}/merge_into/sis_user_id:#{row['merged']}"
-	merge_api = Typhoeus::Request.new(api_call,
-										method: :put,
-										headers: { Authorization: "Bearer #{access_token}" })
-		merge_api.on_complete do |response|
-			if response.code.eql?(200)
-				puts "Merged user #{row['to_be_merged']} into #{row['merged']}"
-			else
-				puts "Unable to merge #{row['to_be_merged']} into #{row['merged']}, check to verify users exist with correct SIS ID values."
-			end
+  api_call = "#{url}/api/v1/users/sis_user_id:#{row['to_be_merged']}/merge_into/sis_user_id:#{row['merged']}"
+	merge_api = Typhoeus::Request.new(api_call, method: :put, headers: { Authorization: "Bearer #{access_token}" })
+	merge_api.on_complete do |response|
+		if response.code.eql?(200)
+			puts "Merged user #{row['to_be_merged']} into #{row['merged']}"
+		else
+			puts "Unable to merge #{row['to_be_merged']} into #{row['merged']}, check to verify users exist with correct SIS ID values."
 		end
+	end
 	hydra.queue(merge_api)
 end
 hydra.run
