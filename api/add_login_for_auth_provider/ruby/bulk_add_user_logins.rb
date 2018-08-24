3ad22b475fae6afe94c0785ef5734b8a6884745f
Colin Cromar <colincromar@gmail.com>
2018-08-23 22:57:03 -0600

    cleans up bulk_add_user_logins.rb (#100)

--- api/add_login_for_auth_provider/ruby/bulk_add_user_logins.rb
+++ api/add_login_for_auth_provider/ruby/bulk_add_user_logins.rb
@@ -1,4 +1,4 @@
-# Working as of 3/11/2018
+# Working as of 8/23/2018
 require 'typhoeus'
 require 'csv'
 require 'json'
@@ -7,32 +7,32 @@ require 'json'
 
 ## ENV
 puts "For prod, hit enter. For Beta, enter 'beta'. For Test, enter 'test'"
-@env = gets.chomp!.downcase
-@env != '' ? @env << '.' : @env
+env = gets.chomp!.downcase
+env != '' ? env << '.' : env
 
 ## DOMAIN
 puts 'Enter the domain, EX: <domain>.instructure.com'
-@domain = gets.chomp!.downcase
+domain = gets.chomp!.downcase
 
 ## TOKEN
 puts 'Enter a valid access token to perform the API calls within this script'
-@token = gets.chomp!
+token = gets.chomp!
 
-## CSV FILE ##
+## CSV FILE
 puts 'Enter the full file path for CSV data. EX: /Users/person/file/to/path.csv'
-@csv_file = gets.chomp!
+csv_file = gets.chomp!
 
-@base_url = "https://#{@domain}.#{@env}instructure.com/api/v1/accounts/self/logins/"
-@default_headers = { Authorization: 'Bearer ' + @token }
+base_url = "https://#{domain}.#{env}instructure.com/api/v1/accounts/self/logins/"
+default_headers = { Authorization: 'Bearer ' + token }
 
-CSV.foreach(@csv_file, headers: true) do |row|
+CSV.foreach(csv_file, headers: true) do |row|
   return raise "Invalid CSV headers: 'canvas_user_id' not found" if row['canvas_user_id'].nil?
   return raise "Invalid CSV headers: 'login_id' not found" if row['login_id'].nil?
   # return raise "Invalid CSV headers: 'authentication_provider_id' not found" if row['authentication_provider_id'].nil? # || ENABLE IF SETTING AUTHENTICAION PROVIDER
   # return raise "Invalid CSV headers: 'sis_user_id' not found" if row['sis_user_id'].nil? # || ENABLE IF NEW LOGIN NEEDS TO HAVE AN SIS ID
   response = Typhoeus.post(
-    @base_url,
-    headers: @default_headers,
+    base_url,
+    headers: default_headers,
     body: {
       user: {
         id: row['canvas_user_id']
@@ -47,5 +47,9 @@ CSV.foreach(@csv_file, headers: true) do |row|
   )
   # parse JSON data to save in readable array
   data = JSON.parse(response.body)
-  puts data
+  if [200, 201].include?(response.code)
+    puts "#{row['canvas_user_id']} successfully updated"
+  else
+    puts "#{row['canvas_user_id']} failed"
+  end
 end
