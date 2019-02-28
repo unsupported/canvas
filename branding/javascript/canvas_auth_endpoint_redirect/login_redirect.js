de7b1ffbca44a55581ad2cf83221614a8cecd152
snelson82 <10576236+snelson82@users.noreply.github.com>
2019-02-28 10:09:15 -0700

    Adds JS login_redirect for Canvas auth endpoint

---
+++ branding/javascript/canvas_auth_endpoint_redirect/login_redirect.js
@@ -0,0 +1,7 @@
+$(function () {
+  if (window.location.pathname == "/login/canvas") {
+    window.location.href = // YOUR REDIRECT URL HERE;
+  } else {
+    console.log("All good. We're in the right place.");
+  }
+});
