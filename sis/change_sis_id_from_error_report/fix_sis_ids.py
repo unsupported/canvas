d6a5d20708bae70109fca58401297a076b63b9de
CaseyInHaengsin <9088410+CaseyInHaengsin@users.noreply.github.com>
2019-11-08 08:01:58 -0700

    Housekeeping (#116)
    * Removed unused data parameter.
    
    * Removed unused data variable and corrected spelling mistake.
    
    * Added last working date.
    
    * updated README to clarify the value used for login_id
    
    * Refactored and simplified change_sis_id_from_error_report script.
    
    * Upgraded bulk avatar python script to python3 and simplified script for ease-of-use. Fixed error in fix_sis_id script

--- sis/change_sis_id_from_error_report/fix_sis_ids.py
+++ sis/change_sis_id_from_error_report/fix_sis_ids.py
@@ -1,9 +1,7 @@
-
 import csv
 import re
 import string
 #This is the name of the error report and is assumed to be in the same directory you are running the script in
-error_report = ''
 #This value is used to write a new CSV
 name_of_fixed_csv = ''
 #This is the the sis_id you would like to change. Values accepted are account, term, course, section, group, group_category, user
@@ -37,9 +35,38 @@ def find_between_r(s, first, last):
     except ValueError:
         return ""
 
+def main():
+    ##Enter name of Error Report generated from Canvas
+    error_report = ''
+    total_val = read_csv(error_report)
+    total_val.pop(0)
+    final_test = []
+    for item in total_val:
+        try:
+            final_test.append(get_info(item))
+        except:
+            print('Failed at {}'.format(item))
+    #Write CSV
+    with open(name_of_fixed_csv, 'w') as write_csv:
+        csv_writer = csv.writer(write_csv)
+        csv_writer.writerow(['old_id', 'new_id', 'type'])
+        for data in final_test:
+            with open(name_of_fixed_csv, 'a') as writ:
+                csv_writ = csv.writer(writ)
+                csv_writer.writerow([data[0], data[1], data[2]])
+
+##Trying to use this with new, better code
+def find_between_r(s, first, last):
+    try:
+        start = s.rindex(first) + len(first)
+        end = s.rindex(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
 #The read_csv function is reading the provisioning report with errors and saving it in an list
-def read_csv():
-    with open(error_report, 'r') as csv_file:
+def read_csv(csv_name):
+    with open(csv_name, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         message = []
         for line in csv_reader:
@@ -51,8 +78,8 @@ def get_info(whole_string):
     while whole_string:
         try:
             curr_id = find_between_r(whole_string, 'SIS ID', 'has already')
-            new_sis_id = find_between_r(whole_string, 'already claimed', 'user_id requested')
-            val = [curr_id, new_sis_id, type]
+            new_sis_id = find_between_r(whole_string, 'already claimed', '\'s user_id requested')
+            val = [curr_id.strip(), new_sis_id.strip(), type]
             return val
         except:
             print('Error at string {stri}'.format(stri=split_string))
