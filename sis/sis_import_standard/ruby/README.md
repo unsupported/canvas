ce2df8cd845529c1d53e9313d2790dd52cb3b799
snelson82 <10576236+snelson82@users.noreply.github.com>
2019-12-17 09:33:01 -0700

    + updated sis_script.rb to use typhoeus gem + syntax update for README + updated Gemfile to include typhoeus gem

--- sis/sis_import_standard/ruby/README.md
+++ sis/sis_import_standard/ruby/README.md
@@ -1,26 +1,30 @@
 # Ruby 2+ SIS Integration Example
+
 This folder includes examples of code that can be used to integrate SIS systems with Canvas. These are relatively simple but can serve as a framework on which you can build.
 
 This script requires the following to be true:
 
-* Ruby 2.0.0+ installed.
-* You know how to schedule tasks to run regularly with crontab (\*nix).
+- Ruby 2.0.0+ installed.
+- You know how to schedule tasks to run regularly with crontab (\*nix).
 
 ## Setup/Installation
-* Copy the scripts to the desired location
-* Edit the script
-  * Change the `access_token` variable
-  * Change the `domain` variable
-  * Change the `env` variable
-  * Change the `source_folder` variable
-  * Change the `archive_folder` variable
+
+- Copy the scripts to the desired location
+- Edit the script
+  - Change the `access_token` variable
+  - Change the `domain` variable
+  - Change the `env` variable
+  - Change the `source_folder` variable
+  - Change the `archive_folder` variable
 
 ## Usage
+
 Create the cron job or scheduled task to run regularly. For example to run this hourly you could create a job like this:
 
     0 * * * * /path/to/sis_script.rb -e
 
 ## Support
+
 As always, this is provided AS-IS, without warranty, and without any support beyond this document and anyone kind enough to help from the community.
 
 This is an unsupported, community-created project. Keep that in mind. Instructure won't be able to help you fix or debug this. That said, the community will hopefully help support and keep both the script and this documentation up-to-date.
--- sis/sis_import_standard/ruby/sis_script.rb
+++ sis/sis_import_standard/ruby/sis_script.rb
@@ -1,89 +1,81 @@
-require 'fileutils'
+# Working as of 12/17/2019
+require 'zip'
 require 'date'
 require 'json'
-require 'unirest'
-require 'zip'
+require 'typhoeus'
+require 'fileutils'
 
 #=======================================
 # Edit this so you don't have to fill it in every time.
-access_token = ''
-domain = ''
-env = '' # Leave empty for production, or use 'test' or 'beta'
-source_folder = ''
-archive_folder = ''
+$access_token = ''
+$domain = ''
+$env = '' # Leave empty for production, or use 'test' or 'beta'
+$source_folder = ''
+$archive_folder = ''
 
 # Don't edit from here down unless you know what you're doing.
 #=======================================
-env != '' ? env << '.' : env
-test_url = "https://#{domain}.#{env}instructure.com/api/v1/accounts/self"
-endpoint_url = "#{test_url}/sis_imports.json?import_type=instructure_csv"
+$env != '' ? $env << '.' : $env
+$test_url = "https://#{$domain}.#{$env}instructure.com/api/v1/accounts/self"
+$endpoint_url = "#{$test_url}/sis_imports.json?import_type=instructure_csv"
+$headers = { 'Authorization' => "Bearer #{$access_token}" }
 
 # Make generic API call to test token, domain, and env.
-test = Unirest.get(test_url, headers: { "Authorization" => "Bearer #{access_token}" })
-
-unless test.code == 200
-	raise "Error: The token, domain, or env variables are not set correctly"
-end
+test = Typhoeus.get($test_url, headers: $headers)
+raise 'Error: The token, domain, or env variables are not set correctly' unless test.code == 200
 
 # Methods to check if the source_folder works
-unless Dir.exist?(source_folder)
-	raise "Error: source_folder isn't a directory, or can't be located."
-end
+raise "Error: source_folder isn't a directory, or can't be located." unless Dir.exist?($source_folder)
+raise "Error: There are no CSV's in the source directory" unless Dir.entries($source_folder).detect { |f| f.match(/.*(.csv)/) }
 
-unless Dir.entries(source_folder).detect {|f| f.match /.*(.csv)/}
-	raise "Error: There are no CSV's in the source directory"
-end
-
-unless Dir.exist?(archive_folder)
-	Dir.mkdir archive_folder
-	puts "Created archive folder at #{archive_folder}"
+unless Dir.exist?($archive_folder)
+  Dir.mkdir $archive_folder
+  puts "Created archive folder at #{$archive_folder}"
 end
 
 files_to_zip = []
-Dir.foreach(source_folder) { |file| files_to_zip.push(file) }
+Dir.foreach($source_folder) { |file| files_to_zip.push(file) }
 
-zipfile_name = "#{source_folder}/archive.zip"
-Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-	files_to_zip.each do |file|
-		zipfile.add(file, "#{source_folder}/#{file}")
-	end
+$zipfile_name = "#{$source_folder}/archive.zip"
+Zip::File.open($zipfile_name, Zip::File::CREATE) do |zipfile|
+  files_to_zip.each do |file|
+    zipfile.add(file, "#{$source_folder}/#{file}")
+  end
 end
 
-#Push to the CSV API endpoint.
-upload = Unirest.post(endpoint_url,
-	headers: {
-		"Authorization" => "Bearer #{access_token}"
-  },
-	parameters: {
-		attachment: File.new(zipfile_name, "r")
-	}
+# Push to the CSV API endpoint.
+$upload = Typhoeus::Request.new(
+  $endpoint_url,
+  method: :post,
+  headers: $headers,
+  body: { attachment: File.new($zipfile_name, 'r') }
 )
-job = upload.body
-
-import_status_url = "#{test_url}/sis_imports/#{job['id']}"
-
-while job["workflow_state"] == "created" || job["workflow_state"] == "importing"
-	puts "importing"
-	sleep(3)
-
-  import_status = Unirest.get(import_status_url,
-  	headers: {
-  	  "Authorization" => "Bearer #{access_token}"
-    }
-  )
-	job = import_status.body
+$upload.on_complete do |response|
+  @job = JSON.parse(response.body)
+  $id = @job['id']
+  while @job['workflow_state'] == 'created' || @job['workflow_state'] == 'importing'
+    puts 'importing'
+    sleep(3)
+    $import_status.run
+  end
+  puts 'Successfully uploaded files'
+  timestamp = Time.now.to_s.gsub(/\s/, '-').gsub(/:/, '-')
+  FileUtils.mv($zipfile_name, "#{$archive_folder}/archive-#{timestamp}.zip")
+  FileUtils.rm Dir.glob("#{$source_folder}/*")
 end
 
-if job["processing_errors"]
-	File.delete(zipfile_name)
-	raise "An error occurred uploading this file. \n #{job}"
+$import_status = Typhoeus::Request.new(
+  "#{$test_url}/sis_imports/#{$id}",
+  method: :get,
+  headers: $headers
+)
+$import_status.on_complete do |response|
+  @job = JSON.parse(response.body)
+  if @job['processing_errors']
+    puts "Processing Errors: #{@job['processing_errors']}"
+    File.delete($zipfile_name)
+    raise "An error occurred uploading this file. \n #{job}"
+  end
 end
 
-if job["processing_warnings"]
-	puts "Processing Errors: #{job["processing_errors"]}"
-end
-
-puts "Successfully uploaded files"
-timestamp = Time.now.to_s.gsub(/\s/, '-').gsub(/:/, '-')
-FileUtils.mv(zipfile_name, "#{archive_folder}/archive-#{timestamp}.zip")
-FileUtils.rm Dir.glob("#{source_folder}/*")
+$upload.run
