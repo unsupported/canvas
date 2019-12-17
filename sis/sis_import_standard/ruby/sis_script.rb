9af4c026eb62642e5a02b35936da305dc061e37d
Stanley Nelson <10576236+snelson82@users.noreply.github.com>
2019-12-17 11:15:40 -0700

    Sis_import_standard ruby script update (#117)
    * + updated sis_script.rb to use typhoeus gem
    + syntax update for README
    + updated Gemfile to include typhoeus gem
    
    * + replaced global variables with local
    + updated variables to be set via user prompts
    
    * + hide token input in console

--- sis/sis_import_standard/ruby/sis_script.rb
+++ sis/sis_import_standard/ruby/sis_script.rb
@@ -1,89 +1,92 @@
-require 'fileutils'
+# Working as of 12/17/2019
+require 'zip'
 require 'date'
 require 'json'
-require 'unirest'
-require 'zip'
+require 'typhoeus'
+require 'fileutils'
+require 'io/console'
 
-#=======================================
-# Edit this so you don't have to fill it in every time.
-access_token = ''
-domain = ''
-env = '' # Leave empty for production, or use 'test' or 'beta'
-source_folder = ''
-archive_folder = ''
+## TOKEN
+puts 'Enter a valid access token to perform the API calls within this script'
+token = STDIN.noecho(&:gets).chomp!
 
-# Don't edit from here down unless you know what you're doing.
-#=======================================
+## DOMAIN
+puts 'Enter the domain, EX: <domain>.instructure.com'
+domain = gets.chomp!.downcase
+
+## ENV
+puts "For prod, hit enter. For Beta, enter 'beta'. For Test, enter 'test'"
+env = gets.chomp!.downcase
 env != '' ? env << '.' : env
+
+## SOURCE FOLDER
+puts 'Enter the full path to the directory where your SIS import CSV files are located'
+source_folder = gets.chomp!
+
+## ARCHIVE FOLDER
+puts 'Enter the full path to the directory where the current SIS import files will be stored after upload'
+archive_folder = gets.chomp!
+
 test_url = "https://#{domain}.#{env}instructure.com/api/v1/accounts/self"
 endpoint_url = "#{test_url}/sis_imports.json?import_type=instructure_csv"
+headers = { 'Authorization': "Bearer #{token}" }
 
 # Make generic API call to test token, domain, and env.
-test = Unirest.get(test_url, headers: { "Authorization" => "Bearer #{access_token}" })
-
-unless test.code == 200
-	raise "Error: The token, domain, or env variables are not set correctly"
-end
+test = Typhoeus.get(test_url, headers: headers)
+raise 'Error: The token, domain, or env variables are not set correctly' unless test.code == 200
 
 # Methods to check if the source_folder works
-unless Dir.exist?(source_folder)
-	raise "Error: source_folder isn't a directory, or can't be located."
-end
+raise "Error: source_folder isn't a directory, or can't be located." unless Dir.exist?(source_folder)
+raise "Error: There are no CSV's in the source directory" unless Dir.entries(source_folder).detect { |f| f.match(/.*(.csv)/) }
 
-unless Dir.entries(source_folder).detect {|f| f.match /.*(.csv)/}
-	raise "Error: There are no CSV's in the source directory"
-end
-
-unless Dir.exist?(archive_folder)
-	Dir.mkdir archive_folder
-	puts "Created archive folder at #{archive_folder}"
-end
+Dir.mkdir(archive_folder) unless Dir.exist?(archive_folder)
 
 files_to_zip = []
 Dir.foreach(source_folder) { |file| files_to_zip.push(file) }
 
 zipfile_name = "#{source_folder}/archive.zip"
 Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-	files_to_zip.each do |file|
-		zipfile.add(file, "#{source_folder}/#{file}")
-	end
+  files_to_zip.each do |file|
+    zipfile.add(file, "#{source_folder}/#{file}")
+  end
 end
 
-#Push to the CSV API endpoint.
-upload = Unirest.post(endpoint_url,
-	headers: {
-		"Authorization" => "Bearer #{access_token}"
-  },
-	parameters: {
-		attachment: File.new(zipfile_name, "r")
-	}
+# Push to the CSV API endpoint.
+upload = Typhoeus::Request.new(
+  endpoint_url,
+  method: :post,
+  headers: headers,
+  body: { attachment: File.new(zipfile_name, 'r') }
 )
-job = upload.body
 
-import_status_url = "#{test_url}/sis_imports/#{job['id']}"
+import_status = Typhoeus::Request.new(
+  "#{test_url}/sis_imports/#{$id}",
+  method: :get,
+  headers: headers
+)
 
-while job["workflow_state"] == "created" || job["workflow_state"] == "importing"
-	puts "importing"
-	sleep(3)
-
-  import_status = Unirest.get(import_status_url,
-  	headers: {
-  	  "Authorization" => "Bearer #{access_token}"
-    }
-  )
-	job = import_status.body
+import_status.on_complete do |response|
+  @job = JSON.parse(response.body)
+  if @job['processing_errors']
+    puts "Processing Errors: #{@job['processing_errors']}"
+    File.delete(zipfile_name)
+    raise "An error occurred uploading this file. \n #{job}"
+  end
 end
 
-if job["processing_errors"]
-	File.delete(zipfile_name)
-	raise "An error occurred uploading this file. \n #{job}"
+upload.on_complete do |response|
+  @job = JSON.parse(response.body)
+  $id = @job['id']
+  @import_check = import_status.run
+  while @job['workflow_state'] == 'created' || @job['workflow_state'] == 'importing'
+    puts 'importing'
+    sleep(3)
+    @import_check
+  end
+  puts 'Successfully uploaded files'
+  timestamp = Time.now.to_s.gsub(/\s/, '-').gsub(/:/, '-')
+  FileUtils.mv(zipfile_name, "#{archive_folder}/archive-#{timestamp}.zip")
+  FileUtils.rm Dir.glob("#{source_folder}/*")
 end
 
-if job["processing_warnings"]
-	puts "Processing Errors: #{job["processing_errors"]}"
-end
-
-puts "Successfully uploaded files"
-timestamp = Time.now.to_s.gsub(/\s/, '-').gsub(/:/, '-')
-FileUtils.mv(zipfile_name, "#{archive_folder}/archive-#{timestamp}.zip")
-FileUtils.rm Dir.glob("#{source_folder}/*")
+upload.run
