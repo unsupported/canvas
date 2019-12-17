816ea45f4ceb9a8046198c5b28c2cec4092eec30
Stanley Nelson <10576236+snelson82@users.noreply.github.com>
2019-12-17 13:14:31 -0700

    Update users timezones updated (#118)
    - removed sample data from example CSV
    + updated README with complete steps for new prompt style
    + updated variables to be set by user prompt

--- api/update_users_timezone/ruby/README.md
+++ api/update_users_timezone/ruby/README.md
@@ -1,17 +1,20 @@
 # Update Users Timezones
 
-This script will allow you to update users time zones en masse for a set of users.
+Allows you to update users time zones in bulk for a set of users.
 
-# Setup
+## Setup
 
-Please see the example CSV `example.csv` for construction of the CSV file.
-The CSV file will need to contain two columns, a column that has the header as `user_sis_id`, where the users SIS ID is added.
-The second column will need to be named `timezone` and have the timezone in the [IANA time zones](http://www.iana.org/time-zones) or the [Rails](http://api.rubyonrails.org/classes/ActiveSupport/TimeZone.html) version.
+- Please see the [example CSV](./example.csv) as an example of the required CSV headers.
+- Required Headers:
+  - `user_sis_id` - The user's SIS ID as it appears in Canvas
+  - `timezone` - The desired timezone formatted either as a [IANA time zone](http://www.iana.org/time-zones) or [Rails](http://api.rubyonrails.org/classes/ActiveSupport/TimeZone.html) timezone.
+    - `America/Denver` is an example a timezone equivalent to `Mountain Time (US & Canada)`
+    - `America/New_York` is an example a timezone equivalent to `Eastern Time (US & Canada)`
 
-You can use `America/Denver` to set the timezone to Mountain Time (US & Canada), or `America/New_York` for Eastern Time (US & Canada).
+## Run
 
-Update the values above the specified line to run the script.
-
-# Run
-
-You will need to run `ruby update_users_timezone.rb` once the variables are set in the file.
+1. Ensure your CSV file has the required `user_sis_id` and `timezone` headers.
+2. In a terminal window: `ruby update_users_timezone.rb`.
+3. Prompts will ask you to separately enter a `token` (valid for domain and environment for which the script will run), `domain`, `environment`, and `source file` in order to set variables within the script.
+4. Terminal output should show `Updated user <user_sis_id> with new time zone settings` for each user in your CSV file.
+5. Last output line should read `Completely Done`.
--- api/update_users_timezone/ruby/update_users_timezone.rb
+++ api/update_users_timezone/ruby/update_users_timezone.rb
@@ -1,35 +1,52 @@
-# Working as of 08/30/2018
-require 'typhoeus'
+# Working as of 12/17/2019
 require 'csv'
+require 'json'
+require 'typhoeus'
+require 'io/console'
 
-# variables that you would want to change to match your domain to update timezones
-access_token = ''
-domain = '' # this would be canvas or canvas.test/canvas.beta
-csv_path = "full/path/to/file.csv"
+## TOKEN
+puts 'Enter a valid access token to perform the API calls within this script'
+token = STDIN.noecho(&:gets).chomp!
 
-########################### DO NOT EDIT ##############################
-url = "https://#{domain}.test.instructure.com/api/v1/" # remove or add test & beta if needed
+## DOMAIN
+puts 'Enter the domain, EX: <domain>.instructure.com'
+domain = gets.chomp!.downcase
+
+## ENV
+puts "For prod, hit enter. For Beta, enter 'beta'. For Test, enter 'test'"
+env = gets.chomp!.downcase
+env != '' ? env << '.' : env
+
+## SOURCE FILE
+puts 'Enter the full path to the file containing your user_sis_ids and timezones'
+source_folder = gets.chomp!
+
+url = "https://#{domain}.#{env}instructure.com/api/v1/" # remove or add test & beta if needed
 
 hydra = Typhoeus::Hydra.new(max_concurrency: 10)
 
-CSV.foreach(csv_path, { headers: true }) do |row|
+CSV.foreach(source_folder, headers: true) do |row|
+  put_response = Typhoeus::Request.new(
+    "#{url}users/sis_user_id:#{row['user_sis_id']}",
+    method: :put,
+    params: {
+      'user[time_zone]': row['timezone']
+    },
+    headers: {
+      authorization: "Bearer #{token}"
+    }
+  )
 
-	put_response = Typhoeus::Request.new("#{url}users/sis_user_id:#{row['user_sis_id']}",
-	method: :put,
-	params: { 'user[time_zone]' => row['timezone'] },
-	headers: { Authorization: "Bearer #{access_token}" }
-	)
-
-	put_response.on_complete do |response|
-		if response.code.eql?(200)
-			puts "Updated user #{row['user_sis_id']} with new time zone settings."
-		elsif response.code != 200
-			puts response.body
-		else
-			puts "Unable to update user #{row['user_sis_id']}'s time zone value."
-		end
-	end
-	hydra.queue(put_response)
+  put_response.on_complete do |response|
+    if response.success?
+      puts "Updated user #{row['user_sis_id']} with new time zone settings."
+    elsif response.code != 200
+      puts response.body
+    else
+      puts "Unable to update user #{row['user_sis_id']}'s time zone value."
+    end
+  end
+  hydra.queue(put_response)
 end
 
 hydra.run
