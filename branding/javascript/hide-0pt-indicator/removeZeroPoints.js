9461c5e8bfea793e9e9e3ca66532441ecb609097
alcarey14 <53814908+alcarey14@users.noreply.github.com>
2020-04-27 11:58:48 +0100

    Update removeZeroPoints.js

---
+++ branding/javascript/hide-0pt-indicator/removeZeroPoints.js
@@ -0,0 +1,35 @@
+// Written by A. Carey, Instructure Inc
+// 2020.04.27 - Forgot/Set password
+// If you put this in your Theme's JS - 
+// It will hide any module point indicators with '0 pts'
+
+
+
+function onElementRendered(selector, cb, _attempts) {
+  var el = $(selector);
+  _attempts = ++_attempts || 1;
+  if (el.length) return cb(el);
+  if (_attempts == 60) return;
+  setTimeout(function() {
+    onElementRendered(selector, cb, _attempts);
+  }, 250);
+};
+
+function removeZeroPoints(){
+
+	let zeropoints = $('.points_possible_display:contains("0 pts")'); // select all points indicators that have 0 pts
+	zeropoints.parent('div').remove(); // remove the parent divs of selected points indicators
+
+}
+
+onElementRendered(".points_possible_display:contains('0 pts')", function(e) {
+  	// code goes here to wait for all elements to load
+  	// function will run if currently on modules page
+
+	if(window.location.href.indexOf("/modules") > -1){
+		removeZeroPoints(); //run removeZeroPoints function
+	}	
+	
+});
+
+
