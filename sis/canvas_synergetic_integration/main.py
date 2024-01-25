7f5bbd7c4325e4dccb593da50f02e85fc8248f85
Geoff Crompton <geoffc@trinity.unimelb.edu.au>
2024-01-25 16:49:42 +1100

    Refactoring away DRY code.
    The functions that execute the SQL statemetns all do exactly the same thing, but with different parameters. So I'm refactoring that data into a data structre, and just writing the code once.

--- sis/canvas_synergetic_integration/main.py
+++ sis/canvas_synergetic_integration/main.py
@@ -14,8 +14,22 @@ import pyodbc
 import datetime
 from contextlib import contextmanager
 import logging, logging.handlers
+from dataclasses import dataclass
+from typing import List
 
 logger = logging.getLogger('canvas_SI')
+
+
+@contextmanager
+def db_cursor(conn):
+    '''
+    Context manager for database cursor. Allows for
+    complete cursor close on failure.
+    '''
+    
+    cur = conn.cursor()
+    yield cur
+    cur.close()
 
 @contextmanager
 def db_connect(conn_string):
@@ -24,143 +38,102 @@ def db_connect(conn_string):
     complete connection close on failure.
     '''
     cnxn = pyodbc.connect(conn_string)
-    cur = cnxn.cursor()
-    yield cur
+    yield cnxn
     cnxn.close()
 
-def get_canvas_accounts(conn_string):
-    '''
-    Selects canvas accounts from uvCanvasAccounts
-    '''
-    with db_connect(conn_string) as cur:
-      cur.execute("select * from uvCanvasAccounts ORDER BY parent_account_id")
-      canvas_accounts = cur.fetchall()
 
-      results = [[
-          'account_id',
-          'parent_account_id',
-          'name',
-          'status'
-      ]]
+@dataclass
+class SIS_Datatype:
+    name: str
+    sql_view: str
+    result_columns : List[str]
 
-      for row in canvas_accounts:
-          results.append(row)
+    def run(self, cur):
+        cur.execute(self.sql_view)
 
-      return results
+        results = self.results_columns
+        for row in cur.fetch():
+            results.append(row)
+        return results
 
-def get_canvas_users(conn_string):
-    '''
-    Selects canvas users from uvCanvasUsers
-    '''
-    with db_connect(conn_string) as cur:
-      cur.execute("select * from uvCanvasUsers")
-      canvas_accounts = cur.fetchall()
+views = [
+    SIS_Datatype(
+        name="canvas_accounts",
+        sql_view="select * from uvCanvasAccounts ORDER BY parent_account_id", 
+        result_columns = [
+            'account_id',
+            'parent_account_id',
+            'name',
+            'status',
+        ]
+    ),
+    SIS_Datatype(
+        name="canvas_users",
+        sql_view="select * from uvCanvasUsers", 
+        result_columns = [
+            'user_id',
+            'login_id',
+            'first_name',
+            'last_name',
+            'full_name',
+            'sortable_name',
+            'email',
+            'declared_user_type',
+            'status',
+        ]
+    ),
+    SIS_Datatype(
+        name="canvas_courses",
+        sql_view="SELECT * FROM uvCanvasCourses", 
+        result_columns = [
+            'course_id',
+            'short_name',
+            'long_name',
+            'account_id',
+            'term_id',
+            'status',
+            'course_format'
+        ]
+    ),
 
-      results = [[
-          'user_id',
-          'login_id',
-          'first_name',
-          'last_name',
-          'full_name',
-          'sortable_name',
-          'email',
-	  'declared_user_type',
-          'status'
-      ]]
+    SIS_Datatype(
+        name="canvas_enrolments",
+        sql_view="select * from uvCanvasEnrollments", 
+        result_columns = [
+            'course_id',
+            'start_date',
+            'end_date',
+            'user_id',
+            'role',
+            'section_id',
+            'status'
+        ]
+    ),  
 
-      for row in canvas_accounts:
-          results.append(row)
+    SIS_Datatype(
+        name="canvas_terms",
+        sql_view="select * from uvCanvasTerms", 
+        result_columns = [
+        'term_id',
+        'name',
+        'status',
+        'start_date',
+        'end_date'
+        ]
+    ),
+ 
+    SIS_Datatype(
+        name="canvas_sections",
+        sql_view="select * from uvCanvasSections", 
+        result_columns = [
+            'section_id',
+            'course_id',
+            'name',
+            'status'
+        ]
+    ),
 
-      return results
-
-def get_canvas_courses(conn_string):
-    '''
-    Selects canvas courses from uvCanvasCourses
-    '''
-    with db_connect(conn_string) as cur:
-      cur.execute("SELECT * FROM uvCanvasCourses")
-      canvas_accounts = cur.fetchall()
-
-      results = [[
-          'course_id',
-          'short_name',
-          'long_name',
-          'account_id',
-          'term_id',
-          'status',
-          'course_format'
-      ]]
-
-      for row in canvas_accounts:
-          results.append(row)
-
-      return results
-
-def get_canvas_enrolments(conn_string):
-    '''
-    Selects canvas enrolments from uvCanvasEnrollments
-    '''
-    with db_connect(conn_string) as cur:
-      cur.execute("select * from uvCanvasEnrollments")
-      canvas_accounts = cur.fetchall()
-
-      results = [[
-          'course_id',
-	  'start_date',
-	  'end_date',
-          'user_id',
-          'role',
-          'section_id',
-          'status'
-        ]]
-
-      for row in canvas_accounts:
-          results.append(row)
-
-      return results
-
-def get_canvas_terms(conn_string):
-    '''
-    Selects canvas enrolments from uvCanvasTerms
-    '''
-    with db_connect(conn_string) as cur:
-      cur.execute("select * from uvCanvasTerms")
-      canvas_terms = cur.fetchall()
-
-      results = [[
-          'term_id',
-	  'name',
-	  'status',
-	  'start_date',
-	  'end_date'
-         
-        ]]
-
-      for row in canvas_terms:
-          results.append(row)
-
-      return results
-
-def get_canvas_sections(conn_string):
-    '''
-    Selects canvas enrolments from uvCanvasSections
-    '''
-    with db_connect(conn_string) as cur:
-      cur.execute("select * from uvCanvasSections")
-      canvas_sections = cur.fetchall()
-
-      results = [[
-          'section_id',
-	  'course_id',
-	  'name',
-          'status'
-         
-        ]]
-
-      for row in canvas_sections:
-          results.append(row)
-
-      return results
+]
 
 def zipdir(path, ziph):
     '''
@@ -224,40 +197,24 @@ def main():
 
     conn_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
 
-    try:
-        go(conn_string, working_dir, base_url, header, payload)
-    except Exception as e:
-        logger.exception("Unhandled exception while running")
-        raise e
+    with db_connect(conn_string) as conn:
+        try:
+            go(conn, working_dir, base_url, header, payload)
+        except Exception as e:
+            logger.exception("Unhandled exception while running")
+            raise e
 
 
-def go(conn_string, working_dir, base_url, header, payload):
-    # ===========
-    # Create CSVs
-    # ===========
-    with open(working_dir + "accounts.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(get_canvas_accounts(conn_string))
 
-    with open(working_dir + "users.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(get_canvas_users(conn_string))
+def go(conn, working_dir, base_url, header, payload):
+ 
 
-    with open(working_dir + "courses.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(get_canvas_courses(conn_string))
+    for view in views:
+        with db_cursor(conn) as cur:
+            with open(working_dir + view.name, newline='', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow(view.run(cur))
 
-    with open(working_dir + "enrolments.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(get_canvas_enrolments(conn_string))
-
-    with open(working_dir + "terms.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(get_canvas_terms(conn_string))
-
-    with open(working_dir + "sections.csv", "w", newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerows(get_canvas_sections(conn_string))
   
     # =============
     # ZIP Directory
